733c8192a5c89adf97b5abb9e731fdfa8a057eb6
Simone Minasola <Simone@DESKTOP-QGL9BVS.homenet.telecomitalia.it>
2020-01-29 20:35:54 +0100

    Fixed issue for terraform validate command

--- modules/pipeline/backend.tf
+++ modules/pipeline/backend.tf
@@ -2,3 +2,8 @@ provider "github" {
   token        = var.github_token
   organization = var.github_repository_owner
 }
+
+# workaround for 'terraform validate' issue (https://github.com/hashicorp/terraform/issues/21790)
+provider "aws" {
+  region  = "eu-west-1"
+}
--- modules/website/backend.tf
+++ modules/website/backend.tf
@@ -3,6 +3,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# workaround for 'terraform validate' issue (https://github.com/hashicorp/terraform/issues/21790)
+provider "aws" {
+  region  = "eu-west-1"
+}
+
 provider "null" {
 }
 
